c376c7668ce4b5690c9cdd9573b76043205e4c3c
Neema-Charity <ncharityyy@gmail.com>
2024-04-11 01:01:51 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+fetch('http://localhost:3000/Restaurants')
+    .then(response => response.json())
+    .then(data => {
+        // Once the data is fetched, call the initialize function
+        initialize(data);
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
+
+function initialize(restaurantsData) {
+    // Assuming `restaurantsData` is an array of objects containing restaurant data
+    restaurantsData.forEach(restaurant => renderOneRestaurant(restaurant));
+}
+
+function renderOneRestaurant(restaurant) {
+    let card = document.createElement('ol');
+    card.className = 'card';
+    card.innerHTML = `
+        <img src="${restaurant.image}" alt="Restaurant Image">
+        <div>
+        <h2>${restaurant.name}</h2>
+        <h3>${restaurant.aesthetic}</h3>
+        <h4>${restaurant.location}</h4>
+        </div>
+    `;
+    document.getElementById('restaurantList').appendChild(card);
+} 
+
+
+
